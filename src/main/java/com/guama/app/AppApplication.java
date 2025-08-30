0988583af2a4ee72c89c61db11191b6e2e58ec41
Carlos Lozano <carlos.lozano@globallogic.com>
2025-08-30 17:47:44 -0500

    Initial commit

---
+++ src/main/java/com/guama/app/AppApplication.java
@@ -0,0 +1,13 @@
+package com.guama.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
